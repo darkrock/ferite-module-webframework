09623fddff505a44c40fb924261f88cf5c79e56d
Chris Ross <ctr@cention.se>
2008-07-03 12:02:04 +0000

    Tweak

--- Public/Support/Components/ValueSlider/ValueSlider.js
+++ Public/Support/Components/ValueSlider/ValueSlider.js
@@ -1,26 +1,16 @@
-function ComponentValueSlider( id ) {
-	var self = new Component(id);
-	
-	self._defaultState = 'value';
-	self.setState('units', 'seconds');
-	self.setState('partial', false);
-	self.setState('value', 0);
-	self.setState('maximum', 60 * 60 * 24);
-	self.setState('minimum', 0);
-	self.dragging = false;
-	self.startPos = 0;
-	self.lastPos = 0;
-
-	self.incrementValue = function() {
-		if( self.getState('units') == 'seconds' ) {
-			return 1;
-		} else if( self.getState('units') == 'minutes' ) {
-			return 60;
-		} else if( self.getState('units') == 'hours' ) {
-			return (60*60);
-		}
+function BasicFormatter( slider ) {
+	var self = this;
+	self.slider = slider;
+	self.increment = function() {
 		return 1;
 	};
+	self.format= function( value ) {
+		return '' + value + ' ' + self.slider.getState('units');
+	}
+}
+function TimeFormatter( slider ) {
+	var self = this;
+	self.slider = slider;
 	self.two = function(x) {
 		return ((x>9)?"":"0")+x;
 	};
@@ -35,24 +25,51 @@ function ComponentValueSlider( id ) {
 			hours: h % 24,
 			days: d
 		};
+	};	
+	self.increment = function() {
+		if( self.slider.getState('units') == 'seconds' ) {
+			return 1;
+		} else if( self.slider.getState('units') == 'minutes' ) {
+			return 60;
+		} else if( self.slider.getState('units') == 'hours' ) {
+			return (60*60);
+		}
+		return 1;
 	};
-	self.formatTime = function( sec ) {
+
+	self.format = function( sec ) {
 		var bd = self.breakdownTime(sec);
 		return '' + 
-			((self.getState('maximum') > (60 * 60 * 24)) ? bd.days + ' days' : '') + 
-			(self.incrementValue() < (60 * 60 * 24) && (self.getState('maximum') > (60 * 60)) ? ' ' + self.two(bd.hours) + ' hours' : '') + 
-			(self.incrementValue() < (60 * 60) && (self.getState('maximum') > (60)) ? ' ' + self.two(bd.minutes) + ' minutes' : '') + 
+			((self.slider.getState('maximum') > (60 * 60 * 24)) ? bd.days + ' days' : '') + 
+			(self.incrementValue() < (60 * 60 * 24) && (self.slider.getState('maximum') > (60 * 60)) ? ' ' + self.two(bd.hours) + ' hours' : '') + 
+			(self.incrementValue() < (60 * 60) && (self.slider.getState('maximum') > (60)) ? ' ' + self.two(bd.minutes) + ' minutes' : '') + 
 			(self.incrementValue() < 60 ? ' ' + self.two(bd.seconds) + ' seconds' : '');
 	};
-	self.formatOutput = self.formatTime;
+	return self;
+}
+function ComponentValueSlider( id ) {
+	var self = new Component(id);
+	
+	self._defaultState = 'value';
+	self.setState('units', 'seconds');
+	self.setState('partial', false);
+	self.setState('value', 0);
+	self.setState('maximum', 60 * 60 * 24);
+	self.setState('minimum', 0);
+	self.dragging = false;
+	self.startPos = 0;
+	self.lastPos = 0;
+
+	self.formatHelper = new BasicFormatter(self);
+	
 	self.updateVisual = function() {
-		self.node().innerHTML = self.formatOutput(self.getState('value'));
+		self.node().innerHTML = self.formatHelper.format(self.getState('value'));
 	};
 	self.prototypeMouseMove = function( event ) {
 		if (self.dragging) {
 			var coords = self.mouseCoordinates(event);
 			var diff = (coords.x - self.startPos.x);
-			self.setState('value', self.getState('initial-value') + (diff * self.incrementValue()) );
+			self.setState('value', self.getState('initial-value') + (diff * self.formatHelper.incrementValue()) );
 			if( self.getState('value') < self.getState('minimum') ) {
 				self.setState('value', self.getState('minimum') + 1);
 			}
