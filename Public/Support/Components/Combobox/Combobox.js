e428a51baa9da4b2eeb7d897173b260f99e1f5f2
Tobias Eriksson <tobias@cention.se>
2008-11-03 17:00:23 +0000

    New and improved combobox. Two things that would be nice, some kind of icon or button indicating that it is a combobox just not a regular text field. Show the selected item in the list.

---
+++ Public/Support/Components/Combobox/Combobox.js
@@ -0,0 +1,32 @@
+function ComponentCombobox( id ) {
+	var self = ComponentTextfield(id);
+	
+	self.listNode = document.getElementById( id + '_list' );
+	self.showingList = false;
+	
+	self.showList = function() {
+		Position.clone( self.node(), self.listNode, { setWidth: false, setHeight: false, offsetTop: 0 + self.node().clientHeight + 1 } );
+		self.listNode.style.width = self.node().offsetWidth - 2 + 'px';
+		self.listNode.style.display = 'block';
+		self.showingList = true;
+	};
+	
+	self.hideList = function() {
+		self.listNode.style.display = 'none';
+		self.showingList = false;
+	};
+	
+	var previousActivate = self.activate;
+	self.activate = function activate() {
+		self.node().onclick = function( event ) {
+			if( self.showingList ) {
+				self.hideList();
+			} else {
+				self.showList();
+			}
+		};
+		previousActivate();
+	};
+	
+	return self;
+}
