3e29c88f0da1716f8701517d3ab2b62342628987
Tobias Eriksson <tobias@cention.se>
2009-03-24 14:30:04 +0000

    Fix for #1699 - reply-to special function.

--- Public/Support/Components/Combobox/Combobox.js
+++ Public/Support/Components/Combobox/Combobox.js
@@ -1,12 +1,17 @@
 function ComponentCombobox( id ) {
 	var self = ComponentTextfield(id);
 	
+	self.iconNode = document.getElementById( id + '_icon' );
 	self.listNode = document.getElementById( id + '_list' );
 	self.showingList = false;
 	
 	self.showList = function() {
+		var iconWidth = 0;
+		if( self.iconNode ) {
+			iconWidth = self.iconNode.offsetWidth;
+		}
 		Position.clone( self.node(), self.listNode, { setWidth: false, setHeight: false, offsetTop: 0 + self.node().clientHeight + 1 } );
-		self.listNode.style.width = self.node().offsetWidth - 2 + 'px';
+		self.listNode.style.width = self.node().offsetWidth + iconWidth - 1 + 'px';
 		self.listNode.style.display = 'block';
 		self.showingList = true;
 	};
@@ -23,13 +28,23 @@ function ComponentCombobox( id ) {
 	var previousActivate = self.activate;
 	self.activate = function activate() {
 		if( self.getState('list-enabled') ) {
-			self.node().onclick = function( event ) {
-				if( self.showingList ) {
-					self.hideList();
-				} else {
-					self.showList();
-				}
-			};
+			if( self.iconNode ) {
+				self.iconNode.onclick = function( event ) {
+					if( self.showingList ) {
+						self.hideList();
+					} else {
+						self.showList();
+					}
+				};
+			} else {
+				self.node().onclick = function( event ) {
+					if( self.showingList ) {
+						self.hideList();
+					} else {
+						self.showList();
+					}
+				};
+			}
 		}
 		if( ! self.getState('textfield-enabled') ) {
 			self.node().onfocus = function( event ) {
