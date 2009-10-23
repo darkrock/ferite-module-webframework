ddee3a357b0f4f9cf8e2f2f94e344d31622d2d30
Tobias Eriksson <tobias@cention.se>
2009-10-23 14:32:44 +0000

    Some code for rich text in receipts.

--- Public/Resources/Javascript/fckeditor/fckconfig.js
+++ Public/Resources/Javascript/fckeditor/fckconfig.js
@@ -117,19 +117,11 @@ FCKConfig.ToolbarSets["Default"] = [
 ] ;
 
 FCKConfig.ToolbarSets["Cention"] = [
-	['Source','DocProps','-','NewPage','Preview','-','Templates'],
-	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
-	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript','-',
+	'OrderedList','UnorderedList','-','Outdent','Indent','Blockquote','-',
+	'JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
 	'/',
-	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
-	['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote','CreateDiv'],
-	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-	['Link','Unlink','Anchor'],
-	['CentionImage', 'Table','Rule','Smiley','SpecialChar','PageBreak'],
-	'/',
-	['Style','FontFormat','FontName','FontSize'],
-	['TextColor','BGColor'],
-	['FitWindow','ShowBlocks','-','About']		// No comma for the last row.
+	['FontName','FontSize', 'TextColor','BGColor','-','Link','CentionImage']		// No comma for the last row.
 ] ;
 
 FCKConfig.ToolbarSets["Basic"] = [
