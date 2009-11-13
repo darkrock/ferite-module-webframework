a9b1933d78082654017ab270db7fd3b60913c218
Madhuri Debnath <madhuri_debnath@cention.se>
2009-11-13 09:33:03 +0000

    do autocomplete in combobox

--- Public/Support/Components/Combobox/Combobox.js
+++ Public/Support/Components/Combobox/Combobox.js
@@ -1,3 +1,18 @@
+Autocompleter.MCAM = Class.create(Autocompleter.Base, {
+	initialize: function(webframework_component, element, update, options) {
+		this.baseInitialize(element, update, options);
+		this.options.asynchronous  = true;
+		this.webframework_component = webframework_component;
+	},
+	getUpdatedChoices: function() {
+		var self = this;
+		_(this.webframework_component).fireCallbackRequest( 'doAutoComplete', function( value ) {
+			self.updateChoices(value);
+		}, { complete_term: $(this.element).value, extra: _(this.webframework_component).getState('extra') });
+	}
+});
+
+
 function ComponentCombobox( id ) {
 	var self = ComponentTextfield(id);
 	
