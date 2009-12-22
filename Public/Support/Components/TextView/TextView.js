ee45189db9b26487e8c1cd258866e4f8a097f50e
Tobias Eriksson <tobias@cention.se>
2009-12-22 08:06:02 +0000

    Add rich text support to new TextView component.

--- Public/Support/Components/TextView/TextView.js
+++ Public/Support/Components/TextView/TextView.js
@@ -4,5 +4,99 @@ function ComponentTextView( id ) {
 	self.rows = function() { return self.node().rows; };
 	self.columns = function() { return self.node().cols; };
 	
+	self.focus = function() {
+		if( CKEDITOR.instances[id] ) {
+			var editor = CKEDITOR.instances[id];
+			editor.focus();
+		} else {
+			self.node().focus();
+		}
+	};
+	
+	self.updateFormValue = function() {
+		if( CKEDITOR.instances[id] ) {
+			var editor = CKEDITOR.instances[id];
+			editor.setData(self.formValue());
+			editor.updateElement();
+		} else {
+			self.node().value = self.formValue();
+		}
+	};
+	
+	self.textValue = function() {
+		if( CKEDITOR.instances[id] ) {
+			var editor = CKEDITOR.instances[id];
+			editor.updateElement();
+		}
+		self.setState('text-value', self.node().value);
+		return self.getState('text-value');
+	};
+	
+	self.empty = function() {
+		var value = self.textValue();
+		if( CKEDITOR.instances[id] ) {
+			value = value.replace('<br />', '').strip();
+		}
+		return (value ? false : true);
+	};
+	
+	self.setTextValue = function( value ) {
+		var editor = CKEDITOR.instances[id];
+		if( editor && (editor.mode == 'wysiwyg' || editor.mode == 'source') ) {
+			value = value.escapeHTML().replace(/(\r\n|[\r\n])/g, '<br />');
+		}
+		self.setState('text-value', value);
+	};
+	
+	self.setRichTextValue = function( value ) {
+		var editor = CKEDITOR.instances[id];
+		if( !editor || editor.mode == 'plain' ) {
+			value = value.stripTags().unescapeHTML().strip();
+		}
+		self.setState('text-value', value);
+	};
+
+	self.appendText = function( value ) {
+		var editor = CKEDITOR.instances[id];
+		if( editor && (editor.mode == 'wysiwyg' || editor.mode == 'source') ) {
+			value = value.escapeHTML().replace(/(\r\n|[\r\n])/g, '<br />');
+		}
+		self.setState('text-value', self.textValue() + value);
+	};
+	
+	self.appendRichText = function( value ) {
+		var editor = CKEDITOR.instances[id];
+		if( !editor || editor.mode == 'plain' ) {
+			value = value.stripTags().unescapeHTML().strip();
+		}
+		self.setState('text-value', self.textValue() + value);
+	};
+	
+	self.setRichText = function( value ) {
+		if( value ) {
+			self.enableRichText();
+		} else {
+			self.disableRichText();
+		}
+	};
+	
+	self.enableRichText = function() {
+		if( !CKEDITOR.instances[id] ) {
+			CKEDITOR.replace(self.node(), self.getState('ckeditor-config'));
+		}
+	}
+	
+	self.disableRichText = function() {
+		var editor = CKEDITOR.instances[id];
+		if( editor ) {
+			var data = editor.getData();
+			if( editor.mode == 'wysiwyg' || editor.mode == 'source' ) {
+				data = data.stripTags().unescapeHTML().strip();
+			}
+			self.node().value = data;
+			editor.destroy(true);
+		}
+	};
+	
 	return self;
 }
