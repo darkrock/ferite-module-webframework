9296f348253bf5ab37cf3a4b8c4e689944ac577a
Tobias Eriksson <tobias@cention.se>
2010-08-13 14:58:36 +0200

    Do not disable the built in spell checker in the web browser.

--- Public/Resources/Javascript/ckeditor/config.js
+++ Public/Resources/Javascript/ckeditor/config.js
@@ -17,6 +17,7 @@ CKEDITOR.editorConfig = function( config )
 	config.enterMode = CKEDITOR.ENTER_BR;
 	config.shiftEnterMode = CKEDITOR.ENTER_BR;
 	config.toolbarCanCollapse = false;
+	config.disableNativeSpellChecker = false;
 	
 	config.toolbar_Full =
 	[
