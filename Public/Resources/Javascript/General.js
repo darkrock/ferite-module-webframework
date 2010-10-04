7aeac8934a1d34a1071fe4b8ffc7eef61d883378
Chris Ross <ctr@cention.se>
2010-10-04 08:14:28 +0100

    Fix for browser window sizing

--- Public/Resources/Javascript/General.js
+++ Public/Resources/Javascript/General.js
@@ -56,19 +56,20 @@ function findPos(obj) {
 }
 
 function BrowserWindowSize() {
-	var winW = 630, winH = 460;
-
-	if (parseInt(navigator.appVersion)>3) {
-		if (navigator.appName=="Netscape") {
-			winW = window.innerWidth;
-			winH = window.innerHeight;
-		}
-		if (navigator.appName.indexOf("Microsoft")!=-1) {
-			winW = document.body.offsetWidth;
-			winH = document.body.offsetHeight;
-		}
+	var width = 0, height = 0;
+	if( typeof( window.innerWidth ) == 'number' ) {
+		width = window.innerWidth;
+		height = window.innerHeight;
+	} else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+		//IE 6+ in 'standards compliant mode'                                                                                                                                                        
+		width = document.documentElement.clientWidth;
+		height = document.documentElement.clientHeight;
+	} else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+		//IE 4 compatible                                                                                                                                                                            
+		width = document.body.clientWidth;
+		height = document.body.clientHeight;
 	}
-	return [winW, winH];
+	return [width, height];
 }
 
 /*-----------------------------------------------------------------------------------------------*/
