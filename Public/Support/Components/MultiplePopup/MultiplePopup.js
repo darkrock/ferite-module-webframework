48852d52ac0276b17c7523684a9e4d39ce0847b7
Tobias Eriksson <tobias@cention.se>
2011-05-30 12:25:33 +0200

    IE8 fix?

--- Public/Support/Components/MultiplePopup/MultiplePopup.js
+++ Public/Support/Components/MultiplePopup/MultiplePopup.js
@@ -281,11 +281,8 @@ function ComponentMultiplePopup( id ) {
 		while( self.buttonNode.childNodes.length > 0 ) {
 			self.buttonNode.removeChild(self.buttonNode.childNodes[0]);
 		}
+		/*
 		self.buttonNode.appendChild((function() {
-			var span = document.createElement('span');
-			span.onmousedown = span.onselectstart = function() { return false; };
-			span.unselectable = true;
-			span.innerHTML = title;
 			return span;
 		})());
 		self.buttonNode.appendChild((function() {
@@ -293,6 +290,15 @@ function ComponentMultiplePopup( id ) {
 			img.src = WFServerURI + 'Resources/Images/arrow_down.gif';
 			return img;
 		})());
+		*/
+		var span = document.createElement('span');
+		span.onmousedown = span.onselectstart = function() { return false; };
+		span.unselectable = true;
+		span.innerHTML = title;
+		var img = document.createElement('img');
+		img.src = WFServerURI + 'Resources/Images/arrow_down.gif';
+		self.buttonNode.appendChild(span);
+		self.buttonNode.appendChild(img);
 		self.buttonNode.onmouseover = function() {
 			self.buttonNode.childNodes[1].className = 'onmouseover';
 		};
